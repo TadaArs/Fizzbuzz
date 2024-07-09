28443740746be384b8caa824b70eecde7fa34306
Tada Ampornprasert <tada.a@arise.tech>
2024-07-09 21:33:24 +0700

    fizzbuzz test number 15 for level 1

--- lv1/fizzbuzz.ts
+++ lv1/fizzbuzz.ts
@@ -1,30 +1,12 @@
 export const fizzbuzz = (n:number):string=>{
-    if (n == 14){
-        return '14'
-    }
-    if (n == 13){
-        return '13'
-    }
-    if (n == 11){
-        return '11'
-    }
-    if (n == 8){
-        return '8'
-    }
-    if (n == 7){
-        return '7'
-    }
-    if (n == 5 || n == 10){
+    if (n%5 == 0){
+        if (n%3 == 0){
+            return 'fizzbuzz'
+        }
         return 'buzz'
-    }
-    if (n == 4){
-        return '4'
     }
     if (n%3 == 0){
         return 'fizz'
     }
-    if (n == 2){
-        return '2'
-    }
-    return '1'
+    return n.toString()
 }
