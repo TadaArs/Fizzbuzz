493fd168c9f07fdd6d34f58cb00e5c9fb254c354
Tada Ampornprasert <tada.a@arise.tech>
2024-07-09 22:30:34 +0700

    3rd fizzbuzz test number 2 for lv1

--- lv1/fizzbuzz.test.ts
+++ lv1/fizzbuzz.test.ts
@@ -10,5 +10,16 @@ describe('fizzbuzz', ()=>{
 
         //Assert
         expect(result).toEqual('1')
+    }),
+
+    it('should return 2', ()=>{
+        //Arrange
+        const number = 2
+
+        //Act
+        const result = fizzBuzz(number)
+
+        //Assert
+        expect(result).toEqual('2')
     })
 })
