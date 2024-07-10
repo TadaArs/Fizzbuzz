4acce37cc986907831b547683fc611c2e56764a9
Tada Ampornprasert <tada.a@arise.tech>
2024-07-10 10:24:46 +0700

    remove files on lv1 and config gitignore

--- lv1/fizzbuzz.ts
+++
@@ -1,9 +0,0 @@
-export const fizzBuzz = (n:number):string =>{
-    if (n%3 == 0){
-        return (n%5 == 0)?"fizzbuzz":"fizz"
-    }
-    if (n%5 == 0){
-        return "buzz"
-    }
-    return n.toString()
-}
