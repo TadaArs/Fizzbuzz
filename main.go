96416fe798266e97a3836be84bdae84f7acdba5b
TadaArs <tada.a@arise.tech>
2024-07-15 02:42:00 +0700

    level 2 fizzbuzz when input is 15 , 2nd idea

--- main.go
+++ main.go
@@ -4,8 +4,8 @@ import "strconv"
 
 func fizzbuzz(n int) string {
 	word := strconv.Itoa(n)
-	list := [3]string{"fizzbuzz", "buzz", "fizz"}
-	bin := min(n%3, 1) + min(n%5, 1)*2
+	list := [3]string{"fizzbuzz", "fizz", "buzz"}
+	bin, _ := strconv.ParseInt(strconv.Itoa(min(n%3, 1))+strconv.Itoa(min(n%5, 1)), 2, 64)
 	// 01 [3], 10 [5], 00 [15]
 	if n%3 == 0 || n%5 == 0 {
 		return list[bin]
