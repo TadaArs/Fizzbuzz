7249a1e3466255edc40d0b4d23c6ac566ecffbbe
TadaArs <tada.a@arise.tech>
2024-07-15 02:46:43 +0700

    level 3 fizzbuzz when input is 1

---
+++ main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+func TestFizzBuzz(t *testing.T) {
+	t.Run("should return string of 1 when input 1", func(t *testing.T) {
+		input := 1
+		want := "1"
+		get := fizzbuzz(input)
+		if get != want {
+			t.Errorf("want %v but get %v", want, get)
+		}
+	})
+}
